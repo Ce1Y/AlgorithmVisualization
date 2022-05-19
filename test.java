3a8623a4eef3a791f6340a8037e2c6392cd639a5
cachouuu <samwei9102@gmail.com>
2022-05-19 18:19:14 +0800

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ class test {
     public static void main() {
         System.out.println("hello World!!leo");
         System.out.println("hello World!!leo");
-
+        System.out.println("hello World!!samwei");
     }
 }
