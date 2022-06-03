e4c7e39b127e7e93381021c190caefaa6e96c0e1
Ce1Y <sycn02430@gmail.com>
2022-06-03 23:36:01 +0800

    Frame1 revise

--- Frame1.java
+++ Frame1.java
@@ -1,9 +1,15 @@
 package src.classSrc;
 
+import java.util.ArrayList;
+
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicComboBoxUI.ItemHandler;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 public class Frame1 extends JFrame{
 
@@ -15,47 +21,57 @@ public class Frame1 extends JFrame{
     private final JComboBox<String> sortSelectComboBox;
     
     String[] option = {"SORT", "Insert", "Merge", "Bubble", "Quick", "Select"};
+    ArrayList<String> imgSrc = new ArrayList<>();
 
     public Frame1() {
         super("Frame 1");
-        // setLayout(null);
+        setLayout(null);
 
+        // set listener
         ActionListener listener = new MyEventListener();
+        // ItemListener itemListener = new ItemListener();
 
         // set panel
         sortSelectPanel = new JPanel();
         sortSelectPanel.setBackground(Color.black);
-        // sortSelectComboBox.setPreferredSize(preferredSize);
 
         learnPanel = new JPanel();
         learnPanel.setBackground(Color.yellow);
-        learnPanel.setPreferredSize(new Dimension(200, 0));
         
         testPanel = new JPanel();
         testPanel.setBackground(Color.green);
-        testPanel.setPreferredSize(new Dimension(200, 0));
 
         // set button
         learnButton = new JButton("Learn");
+        learnButton.setBounds(100, 175, 250, 250);
+        learnButton.setForeground(Color.white);
+        learnButton.setBackground(Color.red);
+        learnButton.setFont(new Font("Helvetica", Font.PLAIN, 24));
         learnButton.addActionListener(listener);
 
         testButton = new JButton("Test");
+        testButton.setBounds(640, 175, 250, 250);
+        testButton.setForeground(Color.white);
+        testButton.setBackground(Color.green);
+        testButton.setFont(new Font("Helvetica", Font.PLAIN, 24));
         testButton.addActionListener(listener);
 
         // set comboBox
-        sortSelectComboBox = new JComboBox<>(option);   
+        sortSelectComboBox = new JComboBox<>(option);
+        sortSelectComboBox.setBounds(400, 40, 200, 50);
+        sortSelectComboBox.setRenderer(new DefaultListCellRenderer() {
+            {
+                setHorizontalAlignment(DefaultListCellRenderer.CENTER);
+            }
+        });
 
         // add new element to panel
-        sortSelectPanel.add(sortSelectComboBox);
 
-        learnPanel.add(learnButton);
-
-        testPanel.add(testButton);
 
         // add new element to frame
-        add(sortSelectPanel, BorderLayout.NORTH);
-        add(learnPanel, BorderLayout.WEST);
-        add(testPanel, BorderLayout.EAST);
+        add(sortSelectComboBox);
+        add(learnButton);
+        add(testButton);
 
     }
 
