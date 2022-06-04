2a18074f7ce3a55524814c40e4fc3e0737134abc
wayne0o0 <wayne901210@gmail.com>
2022-06-04 19:20:26 +0800

    1

--- index.java
+++ index.java
@@ -3,13 +3,12 @@ package src.classSrc;
 import javax.swing.*;
 
 public class index {
-    
 
     public static void main(String[] args) {
         Frame1 frame1 = new Frame1();
         frame1.setSize(1000, 600);
         // set frame1 background
-        ImageIcon background = new ImageIcon("src/imageSrc/whiteBackground.png");
+        ImageIcon background = new ImageIcon("src/imageSrc/background.png");
         JLabel label = new JLabel(background);
         label.setBounds(0, 0, frame1.getWidth(), frame1.getHeight());
         JPanel imagePanel = (JPanel) frame1.getContentPane();
