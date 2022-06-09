823ee2ab5768a8318ceb0e98e03648ff6ed7a3c4
cachouuu <samwei9102@gmail.com>
2022-06-09 14:42:17 +0800

    add Index

--- src/classSrc/Index.java
+++ src/classSrc/Index.java
@@ -2,7 +2,7 @@ package src.classSrc;
 
 import javax.swing.*;
 
-public class GUIFrameTest {
+public class Index {
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
 
