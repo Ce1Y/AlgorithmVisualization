dab9850410befa0ad891d24a49beb150d51d5f9c
leo4545525 <95300026+leo4545525@users.noreply.github.com>
2022-06-09 16:38:38 +0800

    .

---
+++ qsort.java
@@ -0,0 +1,14 @@
+package src.classSrc;
+
+import javax.swing.*;
+
+import javafx.scene.layout.Border;
+
+import java.awt.*;
+
+public class qsort extends JFrame {
+    private JTextField title = new JTextField("Quick Sort");
+    private JPanel myTitle = new JPanel(new Borderlayout());
+    
+
+}
