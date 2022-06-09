eca1b7ae5744b33d7fd570b6a260f79e70ddff0d
wayne0o0 <wayne901210@gmail.com>
2022-06-09 18:05:38 +0800

    1

--- SelectionSort.java
+++ SelectionSort.java
@@ -5,12 +5,13 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class SelectionSort extends JFrame{
-    
+public class SelectionSort extends JFrame {
+
     private JPanel titlePanel;
     private JPanel animatePanel;
     private JPanel codePanel;
     private JButton returnButton;
+    private JTextArea codeText = new JTextArea();
 
     public SelectionSort() {
         super("Selection Sort");
@@ -43,15 +44,17 @@ public class SelectionSort extends JFrame{
         // codePanel.setBackground(Color.GREEN);
         codePanel.setBorder(BorderFactory.createLineBorder(Color.black));
 
-        // add new elements to panel
-
-        JScrollPane panelPane = new JScrollPane(codePanel);
-
+        codeText.append(
+                "#include<bits/stdc++.h>                                            \nusing namespace std\nint main(){\n\n add new elements to panel\n         int A[] = {-3, 6, 5, -1, 0, 9, 3};\n int i, j, tmp;\n int n = sizeof(A)\sizeof(int);\n for(i = n-1; i > 0; i--)\n {\n     for(j = 0; j <= i-1; j++)\n     {\n         if( A[j] > A[j+1])\n         {\n             tmp = A [j];\n             A[j] = A[j+1];\n             A[j+1] = tmp;\n         }\n     }\n }");
+        JScrollPane panelPane = new JScrollPane(codeText);
+        panelPane.setBounds(510, 100, 425, 375);
+        // codePanel.add(panelPane);
         // add new elements to frame
         add(returnButton);
         add(titlePanel);
         add(animatePanel);
-        add(codePanel);
+        add(panelPane);
+        // add(codePanel);
 
         // initialize frame
         setSize(1000, 600);
@@ -61,7 +64,6 @@ public class SelectionSort extends JFrame{
         JPanel imagePanel = (JPanel) getContentPane();
         imagePanel.setOpaque(false);
         getLayeredPane().add(label, Integer.valueOf(Integer.MIN_VALUE));
-
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -76,7 +78,7 @@ public class SelectionSort extends JFrame{
         public void actionPerformed(ActionEvent e) {
             Frame1 frame1 = new Frame1();
             setVisible(false);
-        
+
         }
 
     }
