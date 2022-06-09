76e075bf6e49ff8b1a8b19e81166351038bbfd50
Ce1Y <sycn02430@gmail.com>
2022-06-09 16:46:06 +0800

    beta 1

--- MainFrame.java
+++ MainFrame.java
@@ -31,7 +31,7 @@ public class MainFrame extends JFrame {
         setLayout(new GridLayout(3, 1));
 
         // set background
-        backgroundImage = new ImageIcon("src/imageSrc/background.png");
+        backgroundImage = new ImageIcon("src/imageSrc/cachouuu/Background.png");
         backgroundLabel = new JLabel(backgroundImage);
         backgroundLabel.setBounds(0, 0, this.getWidth(), this.getHeight());
         backgroundPanel = (JPanel) this.getContentPane();
@@ -39,22 +39,22 @@ public class MainFrame extends JFrame {
         this.getLayeredPane().add(backgroundLabel, Integer.valueOf(Integer.MIN_VALUE));
 
         // Chinese title image
-        titleImage = new ImageIcon("src/imageSrc/Title.png");
+        titleImage = new ImageIcon("src/imageSrc/cachouuu/Title.png");
         titleLabel = new JLabel(titleImage);
         titlePanel = new JPanel();
         titlePanel.add(titleLabel);
         add(titlePanel);
 
         // English title image
-        engTitleImage = new ImageIcon("src/imageSrc/TitleENG.png");
+        engTitleImage = new ImageIcon("src/imageSrc/cachouuu/TitleENG.png");
         engTitleLabel = new JLabel(engTitleImage);
         engTitlePanel = new JPanel();
         engTitlePanel.add(engTitleLabel);
         add(engTitlePanel);
 
         // sort and graph button
-        sortImage = new ImageIcon("src/imageSrc/Sort.png");
-        graphImage = new ImageIcon("src/imageSrc/Graph.png");
+        sortImage = new ImageIcon("src/imageSrc/cachouuu/Sort.png");
+        graphImage = new ImageIcon("src/imageSrc/cachouuu/Graph.png");
 
         buttonSort = new JButton(sortImage);
         buttonGraph = new JButton(graphImage);
@@ -69,6 +69,11 @@ public class MainFrame extends JFrame {
 
         buttonSort.addActionListener(handler);
         buttonGraph.addActionListener(handler);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
     }
 
     private class ButtonHandler implements ActionListener {
@@ -76,9 +81,7 @@ public class MainFrame extends JFrame {
         public void actionPerformed(ActionEvent evt) {
             if (evt.getSource() == buttonSort) {
                 Frame1 frame1 = new Frame1();
-                frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame1.setLocationRelativeTo(null);
-                frame1.setVisible(true);
+                setVisible(false);
 
             } else if (evt.getSource() == buttonGraph) {
                 JOptionPane.showMessageDialog(MainFrame.this, "SORRY, IT'S NOT ROLL OUT YET");
