e85696cd4ed512c995dbcab9a75734c8742f0d7a
leo4545525 <95300026+leo4545525@users.noreply.github.com>
2022-06-16 22:48:31 +0800

    qsort

--- QuickSortPanel.java
+++ QuickSortPanel.java
@@ -76,19 +76,15 @@ public class QuickSortPanel extends JPanel {
     int p = 0;
     int r = 9;
     int index;
+    int tmp;
     int index_new;
     int flag = 0;
     int check = 0;
 
-    int pivot = -100;
+    int pivot;
 
     private List<Integer> pivot_pre = new ArrayList<Integer>();
-
     private List<Integer> pivot_next = new ArrayList<Integer>();
-
-    private List<Integer> pivot_pre_new = new ArrayList<Integer>();
-
-    private List<Integer> pivot_next_new = new ArrayList<Integer>();
 
 
     private class TimerAction implements ActionListener, Serializable {
@@ -105,40 +101,45 @@ public class QuickSortPanel extends JPanel {
             switch (currentIndex) {
                 case 1:
                     codeList.setSelectedIndex(currentIndex++);
-                    if (pivot < 0)
+                    if (flag == 0)
                         numbers.get(r).setColor(Color.RED);
-                    else
-                        numbers.get(pivot - 1).setColor(Color.RED);
+                    else {
+                        if (pivot == 0) {
+
+                        } else {
+                            numbers.get(pivot - 1).setColor(Color.RED);
+                        }
+
+                    }
+
                     break;
                 case 2:
                     codeList.setSelectedIndex(currentIndex++);
-                    if (pivot < 0)
+                    if (flag == 0)
                         numbers.get(r).setColor(Color.WHITE);
-                    else
-                        numbers.get(pivot - 1).setColor(Color.WHITE);
-                    break;
-                case 3:
-                    if (pivot < 0)
+                    else {
+                        if (pivot == 0) {
+
+                        } else {
+                            numbers.get(pivot - 1).setColor(Color.WHITE);
+                        }
+                    }
+
+                    if (flag == 0) {
+                        flag = 1;
                         pivot = partition(numbers, p, r);
-                    else if (pivot > 0) {
-                        //這裡會出事
-                        if (pivot_next.get(index) == pivot && flag == 1) {
-                            index--;
-                            while (pivot_next.get(index) > pivot_pre.get(index - 1))
-                                index--;
-                            pivot = partition(numbers, pivot_next.get(index), pivot_pre.get(index - 1));
+                    } else {
+                        if (pivot == 0 ) {
+                            while (pivot_next.get(tmp) >= pivot_pre.get(tmp - 1))
+                                tmp--;
+                            pivot = partition(numbers, pivot_next.get(tmp), pivot_pre.get(tmp - 1));
                         } else {
                             pivot = partition(numbers, p, pivot - 1);
                         }
-
-                    } else if (pivot == 0) {
-                        flag = 1;
-                        while (pivot_next.get(index) > pivot_pre.get(index - 1))
-                            index--;
-                        pivot = partition(numbers, pivot_next.get(index), pivot_pre.get(index - 1));
-                        //p  = index;
                     }
 
+                    break;
+                case 3:
                     codeList.setSelectedIndex(currentIndex++);
                     for (int k = 0; k < numbers.size(); k++) {
                         System.out.print(numbers.get(k).getValue() + "  ");
@@ -146,33 +147,21 @@ public class QuickSortPanel extends JPanel {
                     System.out.println();
                     numbers.get(pivot).setColor(Color.RED);
 
-                    if (flag == 0) {
-                        pivot_pre.add(pivot - 1);
-                        pivot_next.add(pivot + 1);
-                        index = pivot_pre.size() - 1;
-                    }else{
-                        pivot_pre_new.add(pivot - 1);
-                        pivot_next_new.add(pivot + 1);
-                        index_new = pivot_pre_new.size() - 1;
-                    }
+
+                    pivot_pre.add(pivot - 1);
+                    pivot_next.add(pivot + 1);
+                    index = pivot_pre.size() - 1;
+
 
                     System.out.println("pivot = " + pivot);
                     System.out.println("pivot_pre = " + pivot_pre);
                     System.out.println("pivot_next = " + pivot_next);
                     System.out.println("index = " + index);
-                    System.out.println("pivot_pre_new = " +  pivot_pre_new);
-                    System.out.println("pivot_next_new = " + pivot_next_new);
-                    System.out.println("index_new = " + index_new);
-
                     break;
                 case 4:
                     numbers.get(pivot).setColor(Color.BLACK);
                     codeList.setSelectedIndex(currentIndex++);
-                    if (pivot != 0)
-                        currentIndex = 1;
-                    else
-                        currentIndex = 5;
-
+                    currentIndex = 1;
                     break;
                 case 5:
                     codeList.setSelectedIndex(currentIndex++);
@@ -206,26 +195,16 @@ public class QuickSortPanel extends JPanel {
         for (int j = p; j <= r - 1; j++) {
             if (numbers.get(j).getValue() <= x) {
                 i++;
-
                 val1 = numbers.get(i).getValue();
                 val2 = numbers.get(j).getValue();
-
                 numbers.get(i).setValue(val2);
                 numbers.get(j).setValue(val1);
-
-
             }
         }
-
-        //Collections.swap(numbers, numbers.get(i + 1).getValue(), numbers.get(r).getValue());
         val1 = numbers.get(i + 1).getValue();
         val2 = numbers.get(r).getValue();
-
         numbers.get(i + 1).setValue(val2);
         numbers.get(r).setValue(val1);
-        /*for (int k = 0; k < numbers.size(); k++) {
-            System.out.print(numbers.get(k).getValue());
-        }*/
         return i + 1;
 
     }
